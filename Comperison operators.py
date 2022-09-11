26bfb8c3f8c686b6dc314364deaec933dcc6149f
Marhabo <91888254+MarhaboR@users.noreply.github.com>
2022-09-11 23:30:21 +0600

    Add files via upload
    here i'm just practicing using comparison operators in Python

---
+++ Comperison operators.py
@@ -0,0 +1,19 @@
+x = 23; y = 1234
+print('Marhabo lalalla is ', x>y )##x greater than y
+print('Marhabo lalalla is ', x<y )##x less than y
+print('Marhabo lalalla is ', x==y )##x equalls  y
+print('Marhabo lalalla is ', x!=y )##x not equals  y
+print('Marhabo lalalla is ', x>=y )##x greater or equla to  y
+print('Marhabo lalalla is ', x<+y )##x less than or equal to y
+
+m = 10
+o = 15
+print("m + o is ", m+o)
+print("m - o is ", m-o)
+print("m * o is ", m*o)
+print("m // o is ", m//o)
+
+
+x = False
+s = True
+print('x and s is ', x and s)
